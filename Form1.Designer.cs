a2e9a329fc965017035791ecb54c7ac5c75f6345
abdal <abdal@DAMER>
2024-06-17 09:30:03 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace _8Pool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.poolTable1 = new _8Pool.PoolTable();
+            this.poolTable2 = new _8Pool.PoolTable();
+            this.poolTable3 = new _8Pool.PoolTable();
+            this.poolTable4 = new _8Pool.PoolTable();
+            this.poolTable6 = new _8Pool.PoolTable();
+            this.poolTable7 = new _8Pool.PoolTable();
+            this.poolTable5 = new _8Pool.PoolTable();
+            this.poolTable8 = new _8Pool.PoolTable();
+            this.poolTable9 = new _8Pool.PoolTable();
+            this.SuspendLayout();
+            // 
+            // poolTable1
+            // 
+            this.poolTable1.BackColor = System.Drawing.Color.Black;
+            this.poolTable1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.poolTable1.HourlyRate = 10F;
+            this.poolTable1.Location = new System.Drawing.Point(29, 36);
+            this.poolTable1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.poolTable1.Name = "poolTable1";
+            this.poolTable1.Size = new System.Drawing.Size(519, 338);
+            this.poolTable1.TabIndex = 0;
+            this.poolTable1.TablePlayer = "Damer";
+            this.poolTable1.TableTitle = "Table1";
+            this.poolTable1.OnTableComplete += new System.EventHandler<_8Pool.PoolTable.TableCompletedEventArgs>(this.poolTable_OnTableComplete);
+            this.poolTable1.Load += new System.EventHandler(this.poolTable1_Load);
+            // 
+            // poolTable2
+            // 
+            this.poolTable2.BackColor = System.Drawing.Color.Black;
+            this.poolTable2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.poolTable2.HourlyRate = 10F;
+            this.poolTable2.Location = new System.Drawing.Point(29, 393);
+            this.poolTable2.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.poolTable2.Name = "poolTable2";
+            this.poolTable2.Size = new System.Drawing.Size(519, 338);
+            this.poolTable2.TabIndex = 1;
+            this.poolTable2.TablePlayer = "Player4";
+            this.poolTable2.TableTitle = "Table4";
+            this.poolTable2.OnTableComplete += new System.EventHandler<_8Pool.PoolTable.TableCompletedEventArgs>(this.poolTable_OnTableComplete);
+            // 
+            // poolTable3
+            // 
+            this.poolTable3.BackColor = System.Drawing.Color.Black;
+            this.poolTable3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.poolTable3.HourlyRate = 10F;
+            this.poolTable3.Location = new System.Drawing.Point(553, 36);
+            this.poolTable3.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.poolTable3.Name = "poolTable3";
+            this.poolTable3.Size = new System.Drawing.Size(519, 338);
+            this.poolTable3.TabIndex = 2;
+            this.poolTable3.TablePlayer = "Player2";
+            this.poolTable3.TableTitle = "Table2";
+            this.poolTable3.OnTableComplete += new System.EventHandler<_8Pool.PoolTable.TableCompletedEventArgs>(this.poolTable_OnTableComplete);
+            // 
+            // poolTable4
+            // 
+            this.poolTable4.BackColor = System.Drawing.Color.Black;
+            this.poolTable4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.poolTable4.HourlyRate = 10F;
+            this.poolTable4.Location = new System.Drawing.Point(1080, 36);
+            this.poolTable4.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.poolTable4.Name = "poolTable4";
+            this.poolTable4.Size = new System.Drawing.Size(519, 338);
+            this.poolTable4.TabIndex = 3;
+            this.poolTable4.TablePlayer = "Player3";
+            this.poolTable4.TableTitle = "Table3";
+            this.poolTable4.OnTableComplete += new System.EventHandler<_8Pool.PoolTable.TableCompletedEventArgs>(this.poolTable_OnTableComplete);
+            this.poolTable4.Load += new System.EventHandler(this.poolTable4_Load);
+            // 
+            // poolTable6
+            // 
+            this.poolTable6.BackColor = System.Drawing.Color.Black;
+            this.poolTable6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.poolTable6.HourlyRate = 10F;
+            this.poolTable6.Location = new System.Drawing.Point(553, 393);
+            this.poolTable6.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.poolTable6.Name = "poolTable6";
+            this.poolTable6.Size = new System.Drawing.Size(519, 338);
+            this.poolTable6.TabIndex = 5;
+            this.poolTable6.TablePlayer = "Player5";
+            this.poolTable6.TableTitle = "Table5";
+            this.poolTable6.OnTableComplete += new System.EventHandler<_8Pool.PoolTable.TableCompletedEventArgs>(this.poolTable_OnTableComplete);
+            // 
+            // poolTable7
+            // 
+            this.poolTable7.BackColor = System.Drawing.Color.Black;
+            this.poolTable7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.poolTable7.HourlyRate = 10F;
+            this.poolTable7.Location = new System.Drawing.Point(1080, 393);
+            this.poolTable7.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.poolTable7.Name = "poolTable7";
+            this.poolTable7.Size = new System.Drawing.Size(519, 338);
+            this.poolTable7.TabIndex = 6;
+            this.poolTable7.TablePlayer = "Player6";
+            this.poolTable7.TableTitle = "Table6";
+            this.poolTable7.OnTableComplete += new System.EventHandler<_8Pool.PoolTable.TableCompletedEventArgs>(this.poolTable_OnTableComplete);
+            // 
+            // poolTable5
+            // 
+            this.poolTable5.BackColor = System.Drawing.Color.Black;
+            this.poolTable5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.poolTable5.HourlyRate = 10F;
+            this.poolTable5.Location = new System.Drawing.Point(1080, 738);
+            this.poolTable5.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.poolTable5.Name = "poolTable5";
+            this.poolTable5.Size = new System.Drawing.Size(519, 338);
+            this.poolTable5.TabIndex = 9;
+            this.poolTable5.TablePlayer = "Player9";
+            this.poolTable5.TableTitle = "Table9";
+            this.poolTable5.OnTableComplete += new System.EventHandler<_8Pool.PoolTable.TableCompletedEventArgs>(this.poolTable_OnTableComplete);
+            // 
+            // poolTable8
+            // 
+            this.poolTable8.BackColor = System.Drawing.Color.Black;
+            this.poolTable8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.poolTable8.HourlyRate = 10F;
+            this.poolTable8.Location = new System.Drawing.Point(553, 738);
+            this.poolTable8.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.poolTable8.Name = "poolTable8";
+            this.poolTable8.Size = new System.Drawing.Size(519, 338);
+            this.poolTable8.TabIndex = 8;
+            this.poolTable8.TablePlayer = "Player8";
+            this.poolTable8.TableTitle = "Table8";
+            this.poolTable8.OnTableComplete += new System.EventHandler<_8Pool.PoolTable.TableCompletedEventArgs>(this.poolTable_OnTableComplete);
+            // 
+            // poolTable9
+            // 
+            this.poolTable9.BackColor = System.Drawing.Color.Black;
+            this.poolTable9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.poolTable9.HourlyRate = 10F;
+            this.poolTable9.Location = new System.Drawing.Point(29, 738);
+            this.poolTable9.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.poolTable9.Name = "poolTable9";
+            this.poolTable9.Size = new System.Drawing.Size(519, 338);
+            this.poolTable9.TabIndex = 7;
+            this.poolTable9.TablePlayer = "Player7";
+            this.poolTable9.TableTitle = "Table7";
+            this.poolTable9.OnTableComplete += new System.EventHandler<_8Pool.PoolTable.TableCompletedEventArgs>(this.poolTable_OnTableComplete);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1647, 1055);
+            this.Controls.Add(this.poolTable5);
+            this.Controls.Add(this.poolTable8);
+            this.Controls.Add(this.poolTable9);
+            this.Controls.Add(this.poolTable7);
+            this.Controls.Add(this.poolTable6);
+            this.Controls.Add(this.poolTable4);
+            this.Controls.Add(this.poolTable3);
+            this.Controls.Add(this.poolTable2);
+            this.Controls.Add(this.poolTable1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PoolTable poolTable1;
+        private PoolTable poolTable2;
+        private PoolTable poolTable3;
+        private PoolTable poolTable4;
+        private PoolTable poolTable6;
+        private PoolTable poolTable7;
+        private PoolTable poolTable5;
+        private PoolTable poolTable8;
+        private PoolTable poolTable9;
+    }
+}
+
